c76889a517f808bd2c5e93778f36782e669d0914
Riwalenn <riwalenn@gmail.com>
2020-01-12 21:35:12 +0100

    [MAJ] erreurView

--- controller/controller.php
+++ controller/controller.php
@@ -28,7 +28,7 @@ class Controller
         $erreurLine = $erreurManager->getLine();
         $erreurFile = $erreurManager->getFile();
 
-        $view = new View('Erreur');
+        $view = new View('Erreur PDO');
         $view->render('view/errorView.php', ['erreurMessage' => $erreurMessage, 'erreurCode' => $erreurCode, 'erreurLine' => $erreurLine, 'erreurFile' => $erreurFile]);
     }
 
@@ -40,7 +40,7 @@ class Controller
         $erreurLine = $erreurManager->getLine();
         $erreurFile = $erreurManager->getFile();
 
-        $view = new View('Erreur');
+        $view = new View('Erreur Output');
         $view->render('view/errorView.php', ['erreurMessage' => $erreurMessage, 'erreurCode' => $erreurCode, 'erreurLine' => $erreurLine, 'erreurFile' => $erreurFile]);
     }
 
@@ -52,7 +52,7 @@ class Controller
         $erreurLine = $erreurManager->getLine();
         $erreurFile = $erreurManager->getFile();
 
-        $view = new View('Erreur');
+        $view = new View('Exception');
         $view->render('view/errorView.php', ['erreurMessage' => $erreurMessage, 'erreurCode' => $erreurCode, 'erreurLine' => $erreurLine, 'erreurFile' => $erreurFile]);
     }
 
--- view/errorView.php
+++ view/errorView.php
@@ -1,8 +1,10 @@
-<div class="container">
+<section class="bg-light page-section" id="erreurs">
+    <div class="container">
         <div class="row">
             <div class="col-lg-12 text-center">
                 <h2 class="section-heading text-uppercase">Erreur</h2>
-                <p><?php echo  'Message d\'erreur : ' . $erreurMessage . ' (code : ' . $erreurCode . '), ligne ' . $erreurLine . ' dans le fichier ' . $erreurFile; ?></p>
+                <h6 class="alert alert-warning"><?php echo  'Message d\'erreur : ' . $erreurMessage . ' (code : ' . $erreurCode . '), ligne ' . $erreurLine . ' dans le fichier ' . $erreurFile; ?></h6>
             </div>
         </div>
-</div>
+    </div>
+</section>
