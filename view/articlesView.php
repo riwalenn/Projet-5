1709e9ab888ea6208f676bb1594adeb5bcc50502
Riwalenn <riwalenn@gmail.com>
2020-01-15 19:43:16 +0100

    [MAJ] ajout formulaire pour filtres + css

--- view/articlesView.php
+++ view/articlesView.php
@@ -6,17 +6,63 @@
                 <h2 class="section-heading text-uppercase">Articles</h2>
                 <h3 class="section-subheading text-muted">Ci-dessous quelques articles qui pourraient vous
                     intéresser.</h3>
+                <button class="btn filtres btn-sm " type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+                    <i class="fa fa-search"></i> Filtres
+                </button>
+                <div class="collapse" id="collapseExample">
+                    <div class="card card-body shadow-sm p-3 mb-5 bg-white rounded">
+                        Filtres à faire : catégories / périodes / auteurs
+                        <form>
+                            <div class="formulaire-recherche">
+                                <div class="row filtres-form">
+                                    <div class="col">
+                                        <label>Rechercher un auteur :</label>
+                                        <select class="custom-select custom-select-sm">
+                                            <option selected>---indifférent---</option>
+                                        </select>
+                                    </div>
+                                    <div class="col">
+                                        <label>Rechercher une catégorie :</label>
+                                        <select class="custom-select custom-select-sm">
+                                            <option selected>---indifférent---</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="row filtres-form">
+                                    <div class="col">
+                                        <label>Rechercher par date de création :</label>
+                                    </div>
+                                </div>
+                                <div class="row filtres-form">
+                                    <div class="col">
+                                        <input type="date" class="form-control">
+                                    </div>
+                                    <div class="col">
+                                        <input type="date" class="form-control">
+                                    </div>
+                                </div>
+                                <div class="row filtres-form">
+                                    <div class="col">
+                                        <button class="btn filtres-submit btn-sm " type="button">
+                                           Filtrer
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
             </div>
         </div>
         <?php
         if (empty($listPosts)) {
-        ?>
-        <h6 class="alert alert-primary">Nous sommes désolés, la liste est vide ! Merci de revenir plus tard.</h6>
+            ?>
+            <h6 class="alert alert-primary">Nous sommes désolés, la liste est vide ! Merci de revenir plus tard.</h6>
             <?php
-            }
+        }
         ?>
         <div class="row">
-        <?php
+            <?php
             foreach ($listPosts as $posts) {
                 ?>
                 <div class="col-md-4 col-sm-6 portfolio-item">
@@ -84,11 +130,11 @@
                                     <?php
                                 }
                                 foreach ($listComments as $comments) {
-                                ?>
-                                <h2 class="text-uppercase"><?= $comments->getTitle() ?></h2>
-                                <cite title="Auteur" class="item-intro text-muted">Créé par <?= $comments->getPseudo() ?> -
-                                    le <?= $comments->getCreated_at() ?></cite>
-                                <p><?= $comments->getContent() ?></p>
+                                    ?>
+                                    <h2 class="text-uppercase"><?= $comments->getTitle() ?></h2>
+                                    <cite title="Auteur" class="item-intro text-muted">Créé par <?= $comments->getPseudo() ?> -
+                                        le <?= $comments->getCreated_at() ?></cite>
+                                    <p><?= $comments->getContent() ?></p>
                                 <?php } ?>
                             </div>
                         </div>
