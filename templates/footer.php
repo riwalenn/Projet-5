93de097d8302f44bc95a35674bdb1f280d73ad41
Riwalenn <riwalenn@gmail.com>
2021-01-23 22:36:47 +0100

    [MAJ] version 2.0

--- templates/footer.php
+++ templates/footer.php
@@ -66,10 +66,10 @@
                             <h3>1. Présentation du site.</h3>
                             <p>En vertu de l'article 6 de la loi n° 2004-575 du 21 juin 2004 pour la confiance dans
                                 l'économie numérique, il est précisé aux utilisateurs du site <a
-                                        href="<?= htmlspecialchars(Constantes::HTTPS_URL.htmlspecialchars(Constantes::WWW_RIWALENN)) ?>"><?= htmlspecialchars(Constantes::WWW_RIWALENN) ?></a> l'identité des
+                                        href="<?= Constantes::HTTPS_URL.Constantes::WWW_RIWALENN ?>"><?= Constantes::WWW_RIWALENN ?></a> l'identité des
                                 différents intervenants dans le cadre de sa réalisation et de son suivi :</p>
                             <p><strong>Propriétaire</strong> : Riwalenn Bas – 1 rue françois arago<br/>
-                                <strong>Créateur</strong> : <a href="https://<?= htmlspecialchars(Constantes::WWW_RIWALENN) ?>">Riwalenn Bas</a><br/>
+                                <strong>Créateur</strong> : <a href="https://<?= Constantes::WWW_RIWALENN ?>">Riwalenn Bas</a><br/>
                                 <strong>Template</strong> : <a
                                         href="https://github.com/BlackrockDigital/startbootstrap-agency/blob/master/LICENSE">Blackrock
                                     Digital LLC</a><br/>
@@ -88,30 +88,30 @@
                             </p>
 
                             <h3>2. Conditions générales d’utilisation du site et des services proposés.</h3>
-                            <p>L’utilisation du site <a href="<?= htmlspecialchars(Constantes::HTTPS_URL.htmlspecialchars(Constantes::WWW_RIWALENN)) ?>"><?= htmlspecialchars(Constantes::WWW_RIWALENN) ?></a>
+                            <p>L’utilisation du site <a href="<?= Constantes::HTTPS_URL.Constantes::WWW_RIWALENN ?>"><?= Constantes::WWW_RIWALENN ?></a>
                                 implique l’acceptation pleine et entière des conditions générales d’utilisation ci-après
                                 décrites. Ces conditions d’utilisation sont susceptibles d’être modifiées ou complétées
-                                à tout moment, les utilisateurs du site <a href="<?= htmlspecialchars(Constantes::HTTPS_URL.htmlspecialchars(Constantes::WWW_RIWALENN)) ?>"><?= htmlspecialchars(Constantes::WWW_RIWALENN) ?></a>
+                                à tout moment, les utilisateurs du site <a href="<?= Constantes::HTTPS_URL.Constantes::WWW_RIWALENN ?>"><?= Constantes::WWW_RIWALENN ?></a>
                                 sont donc invités à les consulter de manière régulière.</p>
                             <p>Ce site est normalement accessible à tout moment aux utilisateurs. Une interruption pour
                                 raison de maintenance technique peut être toutefois décidée par Riwalenn Bas, qui
                                 s’efforcera alors de communiquer préalablement aux utilisateurs les dates et heures de
                                 l’intervention.</p>
-                            <p>Le site <a href="<?= htmlspecialchars(Constantes::HTTPS_URL.htmlspecialchars(Constantes::WWW_RIWALENN)) ?>"><?= htmlspecialchars(Constantes::WWW_RIWALENN) ?></a> est mis à jour
+                            <p>Le site <a href="<?= Constantes::HTTPS_URL.Constantes::WWW_RIWALENN ?>"><?= Constantes::WWW_RIWALENN ?></a> est mis à jour
                                 régulièrement par Riwalenn Bas. De la même façon, les mentions légales peuvent être
                                 modifiées à tout moment : elles s’imposent néanmoins à l’utilisateur qui est invité à
                                 s’y référer le plus souvent possible afin d’en prendre connaissance.</p>
                             <h3>3. Description des services fournis.</h3>
-                            <p>Le site <a href="<?= htmlspecialchars(Constantes::HTTPS_URL.htmlspecialchars(Constantes::WWW_RIWALENN)) ?>"><?= htmlspecialchars(Constantes::WWW_RIWALENN) ?></a> a pour objet de
+                            <p>Le site <a href="<?= Constantes::HTTPS_URL.Constantes::WWW_RIWALENN ?>"><?= Constantes::WWW_RIWALENN ?></a> a pour objet de
                                 fournir une information concernant l’ensemble des activités de la société.</p>
-                            <p>Riwalenn Bas s’efforce de fournir sur le site <a href="<?= htmlspecialchars(Constantes::HTTPS_URL.htmlspecialchars(Constantes::WWW_RIWALENN)) ?>"><?= htmlspecialchars(Constantes::WWW_RIWALENN) ?></a>
+                            <p>Riwalenn Bas s’efforce de fournir sur le site <a href="<?= Constantes::HTTPS_URL.Constantes::WWW_RIWALENN ?>"><?= Constantes::WWW_RIWALENN ?></a>
                                 des informations aussi précises que possible. Toutefois, il ne pourra être tenue
                                 responsable des omissions, des inexactitudes et des carences dans la mise à jour,
                                 qu’elles soient de son fait ou du fait des tiers partenaires qui lui fournissent ces
                                 informations.</p>
-                            <p>Tous les informations indiquées sur le site <a href="<?= htmlspecialchars(Constantes::HTTPS_URL.htmlspecialchars(Constantes::WWW_RIWALENN)) ?>"><?= htmlspecialchars(Constantes::WWW_RIWALENN) ?></a>
+                            <p>Tous les informations indiquées sur le site <a href="<?= Constantes::HTTPS_URL.Constantes::WWW_RIWALENN ?>"><?= Constantes::WWW_RIWALENN ?></a>
                                 sont données à titre indicatif, et sont susceptibles d’évoluer. Par ailleurs, les
-                                renseignements figurant sur le site <a href="<?= htmlspecialchars(Constantes::HTTPS_URL.htmlspecialchars(Constantes::WWW_RIWALENN)) ?>"><?= htmlspecialchars(Constantes::WWW_RIWALENN) ?></a>
+                                renseignements figurant sur le site <a href="<?= Constantes::HTTPS_URL.Constantes::WWW_RIWALENN ?>"><?= Constantes::WWW_RIWALENN ?></a>
                                 ne sont pas exhaustifs. Ils sont donnés sous réserve de modifications ayant été
                                 apportées depuis leur mise en ligne.</p>
                             <h3>4. Limitations contractuelles sur les données techniques.</h3>
@@ -133,12 +133,12 @@
                                 Intellectuelle.</p>
                             <h3>6. Limitations de responsabilité.</h3>
                             <p>Riwalenn Bas ne pourra être tenue responsable des dommages directs et indirects causés au
-                                matériel de l’utilisateur, lors de l’accès au site <?= htmlspecialchars(Constantes::WWW_RIWALENN) ?>, et résultant
+                                matériel de l’utilisateur, lors de l’accès au site <?= Constantes::WWW_RIWALENN ?>, et résultant
                                 soit de l’utilisation d’un matériel ne répondant pas aux spécifications indiquées au
                                 point 4, soit de l’apparition d’un bug ou d’une incompatibilité.</p>
                             <p>Riwalenn Bas ne pourra également être tenue responsable des dommages indirects (tels par
                                 exemple qu’une perte de marché ou perte d’une chance) consécutifs à l’utilisation du
-                                site <a href="<?= htmlspecialchars(Constantes::HTTPS_URL.htmlspecialchars(Constantes::WWW_RIWALENN)) ?>"><?= htmlspecialchars(Constantes::WWW_RIWALENN) ?></a>.</p>
+                                site <a href="<?= Constantes::HTTPS_URL.Constantes::WWW_RIWALENN ?>"><?= Constantes::WWW_RIWALENN ?></a>.</p>
                             <p>Des espaces interactifs (possibilité de poser des questions dans l’espace contact) sont à
                                 la disposition des utilisateurs. Riwalenn Bas se réserve le droit de supprimer, sans
                                 mise en demeure préalable, tout contenu déposé dans cet espace qui contreviendrait à la
@@ -151,17 +151,17 @@
                             <p>En France, les données personnelles sont notamment protégées par la loi n° 78-87 du 6
                                 janvier 1978, la loi n° 2004-801 du 6 août 2004, l'article L. 226-13 du Code pénal et la
                                 Directive Européenne du 24 octobre 1995.</p>
-                            <p>A l'occasion de l'utilisation du site <a href="<?= htmlspecialchars(Constantes::HTTPS_URL.htmlspecialchars(Constantes::WWW_RIWALENN)) ?>"><?= htmlspecialchars(Constantes::WWW_RIWALENN) ?></a>,
+                            <p>A l'occasion de l'utilisation du site <a href="<?= Constantes::HTTPS_URL.Constantes::WWW_RIWALENN ?>"><?= Constantes::WWW_RIWALENN ?></a>,
                                 peuvent êtres recueillies : l'URL des liens par l'intermédiaire desquels l'utilisateur a
-                                accédé au site <a href="<?= htmlspecialchars(Constantes::HTTPS_URL.htmlspecialchars(Constantes::WWW_RIWALENN)) ?>"><?= htmlspecialchars(Constantes::WWW_RIWALENN) ?></a>, le
+                                accédé au site <a href="<?= Constantes::HTTPS_URL.Constantes::WWW_RIWALENN ?>"><?= Constantes::WWW_RIWALENN ?></a>, le
                                 fournisseur d'accès de l'utilisateur, l'adresse de protocole Internet (IP) de
                                 l'utilisateur.</p>
                             <p> En tout état de cause Riwalenn Bas ne collecte des informations personnelles relatives à
                                 l'utilisateur que pour le besoin de certains services proposés par le site <a
-                                        href="<?= htmlspecialchars(Constantes::HTTPS_URL.htmlspecialchars(Constantes::WWW_RIWALENN)) ?>"><?= htmlspecialchars(Constantes::WWW_RIWALENN) ?></a>. L'utilisateur
+                                        href="<?= Constantes::HTTPS_URL.Constantes::WWW_RIWALENN ?>"><?= Constantes::WWW_RIWALENN ?></a>. L'utilisateur
                                 fournit ces informations en toute connaissance de cause, notamment lorsqu'il procède par
                                 lui-même à leur saisie. Il est alors précisé à l'utilisateur du site <a
-                                        href="<?= htmlspecialchars(Constantes::HTTPS_URL.htmlspecialchars(Constantes::WWW_RIWALENN)) ?>"><?= htmlspecialchars(Constantes::WWW_RIWALENN) ?></a> l’obligation ou non
+                                        href="<?= Constantes::HTTPS_URL.Constantes::WWW_RIWALENN ?>"><?= Constantes::WWW_RIWALENN ?></a> l’obligation ou non
                                 de fournir ces informations.</p>
                             <p>Conformément aux dispositions des articles 38 et suivants de la loi 78-17 du 6 janvier
                                 1978 relative à l’informatique, aux fichiers et aux libertés, tout utilisateur dispose
@@ -170,13 +170,13 @@
                                 d’identité avec signature du titulaire de la pièce, en précisant l’adresse à laquelle la
                                 réponse doit être envoyée.</p>
                             <p>Aucune information personnelle de l'utilisateur du site <a
-                                    href="<?= htmlspecialchars(Constantes::HTTPS_URL.htmlspecialchars(Constantes::WWW_RIWALENN)) ?>"><?= htmlspecialchars(Constantes::WWW_RIWALENN) ?></a> n'est publiée à l'insu
+                                    href="<?= Constantes::HTTPS_URL.Constantes::WWW_RIWALENN ?>"><?= Constantes::WWW_RIWALENN ?></a> n'est publiée à l'insu
                                 de
                                 l'utilisateur, échangée, transférée, cédée ou vendue sur un support quelconque à des
                                 tiers. Seule l'hypothèse du rachat de Riwalenn Bas et de ses droits permettrait la
                                 transmission des dites informations à l'éventuel acquéreur qui serait à son tour tenu de
                                 la même obligation de conservation et de modification des données vis à vis de
-                                l'utilisateur du site <a href="<?= htmlspecialchars(Constantes::HTTPS_URL.htmlspecialchars(Constantes::WWW_RIWALENN)) ?>"><?= htmlspecialchars(Constantes::WWW_RIWALENN) ?></a>.
+                                l'utilisateur du site <a href="<?= Constantes::HTTPS_URL.Constantes::WWW_RIWALENN ?>"><?= Constantes::WWW_RIWALENN ?></a>.
                             </p>
                             <p>Le site n'est pas déclaré à la CNIL car il ne recueille pas d'informations personnelles.
                                 .</p>
@@ -184,12 +184,12 @@
                                 transposant la directive 96/9 du 11 mars 1996 relative à la protection juridique des
                                 bases de données.</p>
                             <h3>8. Liens hypertextes et cookies.</h3>
-                            <p>Le site <a href="<?= htmlspecialchars(Constantes::HTTPS_URL.htmlspecialchars(Constantes::WWW_RIWALENN)) ?>"><?= htmlspecialchars(Constantes::WWW_RIWALENN) ?></a> contient un
+                            <p>Le site <a href="<?= Constantes::HTTPS_URL.Constantes::WWW_RIWALENN ?>"><?= Constantes::WWW_RIWALENN ?></a> contient un
                                 certain
                                 nombre de liens hypertextes vers d’autres sites, mis en place avec l’autorisation de
                                 Riwalenn Bas. Cependant, Riwalenn Bas n’a pas la possibilité de vérifier le contenu des
                                 sites ainsi visités, et n’assumera en conséquence aucune responsabilité de ce fait.</p>
-                            <p>La navigation sur le site <a href="<?= htmlspecialchars(Constantes::HTTPS_URL.htmlspecialchars(Constantes::WWW_RIWALENN)) ?>"><?= htmlspecialchars(Constantes::WWW_RIWALENN) ?></a>
+                            <p>La navigation sur le site <a href="<?= Constantes::HTTPS_URL.Constantes::WWW_RIWALENN ?>"><?= Constantes::WWW_RIWALENN ?></a>
                                 est susceptible de provoquer l’installation de cookie(s) sur l’ordinateur de
                                 l’utilisateur. Un cookie est un fichier de petite taille, qui ne permet pas
                                 l’identification de l’utilisateur, mais qui enregistre des informations relatives à la
@@ -217,7 +217,7 @@
 
                             <h3>9. Droit applicable et attribution de juridiction.</h3>
                             <p>Tout litige en relation avec l’utilisation du site <a
-                                    href="<?= htmlspecialchars(Constantes::HTTPS_URL.htmlspecialchars(Constantes::WWW_RIWALENN)) ?>"><?= htmlspecialchars(Constantes::WWW_RIWALENN) ?></a>
+                                    href="<?= Constantes::HTTPS_URL.Constantes::WWW_RIWALENN ?>"><?= Constantes::WWW_RIWALENN ?></a>
                                 est soumis au droit français. Il est fait attribution exclusive de juridiction aux
                                 tribunaux compétents de Paris.</p>
                             <h3>10. Les principales lois concernées.</h3>
